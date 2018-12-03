597d25bc4da56e39fb15cbdac58418a417ff4f63
ivaylo <ivaylo.mihaylov@botronsoft.com>
2018-12-03 20:51:29 +0200

    regex homework

---
+++ regex-css-selector-hw/expectations.js
@@ -0,0 +1,18 @@
+rexExpExpected = {
+"exercise_1": [["this iS not the last homework of Internet Programming", "Internet"], ["WHAT if there 1s all CAPS Words.", "Words"], ["what if the wOrD is mixed in Another?", "Or"]],
+"exercise_2": [["574398798745398745398475398489287593847593845983749570934857938453940881234567748379289342983477382739488473", "0881234567"], ["8217364827634827346730881234-2389476276340887777777882348727634726773-088088088088088088", "0887777777"], ["01923981238172630880123456732984572389782374928340887654321823467234", "0887654321"]],
+"exercise_3": [["011101000111010101100101011100110010000001101001011100110012000000111010001101000011001010010000001100010011001010111001101110100", "2"], ["01110100011101010110010101110011001000000110l00101110011001000000111010001101000011001010010000001100010011001010111001101110100", "l"], ["01110100011101010110010101110011OO1000000110100101110011001000000111010001101000011001010010000001100010011001010111001101110100", "OO"]],
+"exercise_4": [["validusername", "validusername"], ["valid.username", "valid.username"], ["valid_username", "valid_username"], ["notvalidu$ername", null], ["notv@lidusername", null], [".notvalidusername", null], ["0notvalidusername", null], ["_notvalidusername", null], ["val1dusername", "val1dusername"], ["no", null], ["notvalidnotvalidnotvalidnotvalid", null], ["isV", "isV"]],
+"exercise_5": [["1000", "1000"], ["1500", "1500"], ["1501", null], ["999", "999"], ["2000", null], ["1399", "1399"], ["1499", "1499"]],
+"exercise_6": [["<div class='first second third' />", "class='first second third'"], ["Mercedess S class", null], ["class=12", null], ["<table class=\"someClass\" >", "class=\"someClass\""]],
+};
+cssSelectorsExpected = {
+"exercise_1": ["3-2"],
+"exercise_2": ["4-1-1","4-1-2","4-2-1","4-2-2"],
+"exercise_3": ["3-2-3"],
+"exercise_4": ["3"],
+"exercise_5": ["1-1-2"],
+"exercise_6": ["2-1-1-2-1", "2-1-2-1-1"],
+"exercise_7": ["4-2-2"],
+"exercise_8": ["2"]
+};
---
+++ regex-css-selector-hw/input.js
@@ -0,0 +1,18 @@
+regExps = {
+"exercise_1": /.*/,
+"exercise_2": /.*/,
+"exercise_3": /.*/,
+"exercise_4": /.*/,
+"exercise_5": /.*/,
+"exercise_6": /.*/
+};
+cssSelectors = {
+"exercise_1": "css",
+"exercise_2": "css",
+"exercise_3": "css",
+"exercise_4": "css",
+"exercise_5": "css",
+"exercise_6": "css",
+"exercise_7": "css",
+"exercise_8": "css"
+};
